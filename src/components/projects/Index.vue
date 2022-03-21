34b19820e334e5f2574b97ac11a9dc693155281f
Kristian Engberg <krissoengberg@gmail.com>
2022-03-21 10:52:59 +0100

    Update Index.vue

--- src/components/projects/Index.vue
+++ src/components/projects/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <PreviewTwo v-for=" project  in Database.project" :key="project.title" :project="project" class>
+    <PreviewTwo v-for=" project  in db.project" :key="project.title" :project="project" class>
         <div class="hidden">
             <img :src="project.picture" alt />
         </div>
@@ -11,6 +11,7 @@
 import PreviewOne from "./PreviewOne.vue"
 import PreviewTwo from "./PreviewTwo.vue"
 import Database from '../../database/database.json'
+import { ref } from '@vue/reactivity'
 
 export default {
 
@@ -21,10 +22,20 @@ export default {
 
     setup() {
 
+        const db = ref({})
+
+        fetch('../../database/database.json').then((response) => {
+            return response.json()
+        }).then((data) => {
+            db.value = data
+        }).catch((err) => {
+            console.log(err)
+        })
+
 
 
         return {
-            Database
+            db
         }
     }
 
